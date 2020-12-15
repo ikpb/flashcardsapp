c252239b7ea08a4af2dcc3f537528e1ef3376750
ikpb <ikpb@users.noreply.github.com>
2020-12-15 03:39:03 -0500

    update angular cli to 11

--- src/app/components/log-out-button/log-out-button.component.spec.ts
+++ src/app/components/log-out-button/log-out-button.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LogOutButtonComponent } from './log-out-button.component';
 
@@ -6,7 +6,7 @@ describe('LogOutButtonComponent', () => {
   let component: LogOutButtonComponent;
   let fixture: ComponentFixture<LogOutButtonComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LogOutButtonComponent ]
     })
